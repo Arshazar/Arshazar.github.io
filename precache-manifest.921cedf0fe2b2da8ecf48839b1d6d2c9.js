0773e32f0e1e6278366ed4097e2e95436ac55807
arshavir azarfar <altair.cena@gmail.com>
2020-05-06 20:38:17 +0430

    Updates

---
+++ precache-manifest.921cedf0fe2b2da8ecf48839b1d6d2c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd105e6260645ca622b72e36656adab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb3d2eb2444383ee8f48",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cb05bd9449cc02c3ff01",
+    "url": "/static/css/main.123e448c.chunk.css"
+  },
+  {
+    "revision": "eb3d2eb2444383ee8f48",
+    "url": "/static/js/2.883ce9dc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.883ce9dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb05bd9449cc02c3ff01",
+    "url": "/static/js/main.88199328.chunk.js"
+  },
+  {
+    "revision": "b98d03fd6e723dd4adfc",
+    "url": "/static/js/runtime-main.e484b429.js"
+  },
+  {
+    "revision": "3bfed7341f1b82460681751bfe247f46",
+    "url": "/static/media/Neon Bugler.3bfed734.otf"
+  }
+]);
